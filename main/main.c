5c35e8dd007d8087ac41fc5a9c51cccf0e3e5d55
pridy_ <pridythinking@gmx.de>
2026-04-12 22:50:05 +0200

    Update sdkconfig regarding option to show I2C debug messages; Update main.c with a file description, sections for headers, defines, variables and similar; Add basic log information using a module tag;

--- main/main.c
+++ main/main.c
@@ -1,6 +1,41 @@
+/**
+ * Pridys temperature and humidity measurement project V001
+ * Using a SHT85 via I2C and Non Volatile Memory (FATFS, wear levelling)
+ */
+
+/* ====== Standard includes ====== */
 #include <stdio.h>
 
+/* ====== Framework includes ====== */
+#include "esp_log.h"
+
+/* ====== Proprietary includes ====== */
+
+
+/* ====== Defines ====== */
+//#define asdf 0x01
+
+/* ====== Global variable declaration ====== */
+static const char * module_tag = "ESP_SHT85_NVM_main";
+
+
+/**
+ * Application main function
+ */
 void app_main(void)
 {
+    ESP_LOGI(module_tag, "\n");
+    ESP_LOGI(module_tag, "Application started...\n");
 
+    /* ====== Init section ====== */
+    
+
+    /* ====== Runtime section ====== */
+
+
+    /* ====== De init section ====== */
+    
+
+
+    ESP_LOGI(TAG, "Application stopped.\n");
 }
