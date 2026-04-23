0fa4e80d74571ad14dd703a2246e76065273f2a7
Pridy V <pridythinking@gmx.de>
2026-04-23 21:43:51 +0200

    Stash before restructuring the component by introducing submodules; This file grows and is not maintainable; Partly implemented function for plausibility check is not working correctly at this point, humidity is 100% after heater activation, but it should lower some percentage points; Code added with this commit will be rewritten and submodules will be introduced;

--- components/pridys_SHT85/src/pridys_SHT85.c
+++ components/pridys_SHT85/src/pridys_SHT85.c
@@ -61,12 +61,36 @@ typedef struct{
     uint16_t    temperature_raw_u16;
     float       temperature_f;
     uint8_t     humidity_raw_u8a[2];
+    uint16_t    humidity_raw_u16;
     float       humidity_f;
     uint8_t     crc_received_u8a[2];
     uint8_t     crc_calculated_u8a[2];
 } measurement_SHT85_st;
 
 measurement_SHT85_st measurement_SHT85_s;
+
+typedef struct{
+    bool        temperature_too_high;
+    bool        temperature_too_low;
+    bool        temperature_range_check_passed;
+    bool        humidity_too_high;
+    bool        humidity_too_low;
+    bool        humidity_range_check_passed;
+    bool        heater_diff_temperature_too_low;
+    bool        heater_diff_humidity_too_low;
+    bool        heater_diff_passed;
+} plausibility_check_st;
+
+plausibility_check_st plausibility_check_s;
+
+typedef struct{
+    uint8_t     i2c_receive_data_u8a[3];
+    uint8_t     raw_bytes_u8a[2];
+    uint8_t     crc_received_u8;
+    uint8_t     crc_calculated_u8;
+} status_register_sht85_st;
+
+status_register_sht85_st status_register_sht85_s; 
 
 i2c_master_bus_handle_t bus_handle;
 i2c_master_dev_handle_t dev_handle;
@@ -139,9 +163,28 @@ static void pSHT85_LF_init_sensor_sht85_variables(void)
     memset(measurement_SHT85_s.crc_calculated_u8a, 0xFFu, 2);
 
     measurement_SHT85_s.temperature_raw_u16 = 0xFFFFu;
+    measurement_SHT85_s.humidity_raw_u16 = 0xFFFFu;
 
     measurement_SHT85_s.temperature_f = NAN;
     measurement_SHT85_s.humidity_f = NAN;
+
+    plausibility_check_s.temperature_too_high = true;
+    plausibility_check_s.temperature_too_low = true;
+    plausibility_check_s.temperature_range_check_passed = false;
+
+    plausibility_check_s.humidity_too_high = true;
+    plausibility_check_s.humidity_too_low = true;
+    plausibility_check_s.humidity_range_check_passed = false;
+
+    plausibility_check_s.heater_diff_temperature_too_low = true;
+    plausibility_check_s.heater_diff_humidity_too_low = true;
+    plausibility_check_s.heater_diff_passed = false;
+
+    memset(status_register_sht85_s.i2c_receive_data_u8a, 0xFFu, 3);
+    memset(status_register_sht85_s.raw_bytes_u8a, 0xFFu, 2);
+    status_register_sht85_s.crc_received_u8 = 0xFFu;
+    status_register_sht85_s.crc_calculated_u8 = 0xFFu;
+
 }
 
 /**
@@ -239,10 +282,17 @@ static void pSHT85_LF_read_sensor_sht85_serial(void)
 static void pSHT85_LF_log_sensor_sht85_serial(void)
 {
     ESP_LOGI(module_tag, "Sensor SHT85 serial number:");
+    
+    /*
     ESP_LOGI(module_tag, "\t%X", serial_SHT85_s.serial_u8a[0]);
     ESP_LOGI(module_tag, "\t%X", serial_SHT85_s.serial_u8a[1]);
     ESP_LOGI(module_tag, "\t%X", serial_SHT85_s.serial_u8a[2]);
     ESP_LOGI(module_tag, "\t%X", serial_SHT85_s.serial_u8a[3]);
+    */
+
+    ESP_LOGI(module_tag, "\t%X %X %X %X", 
+        serial_SHT85_s.serial_u8a[0], serial_SHT85_s.serial_u8a[1], 
+        serial_SHT85_s.serial_u8a[2], serial_SHT85_s.serial_u8a[3]);
 }
 
 /**
@@ -307,8 +357,8 @@ static void pSHT85_LF_plausibility_check(void)
         2 bytes serial, CRC; another 2 bytes serial => 4 bytes serial */
     uint8_t i2c_receive_data[6] = {0x00u, 0x00u, 0x00u, 0x00u, 0x00u, 0x00u};
 
-    /* As per the datasheet, command 'Get Serial Number' 
-        is 0x36 82 */
+    /* As per the datasheet, command 'single shot measurement, high repeatibility' 
+        is 0x24 00 */
     uint8_t cmd[2] = {0x24u, 0x00u};
 
 
@@ -351,30 +401,176 @@ static void pSHT85_LF_plausibility_check(void)
     }
 
     ESP_LOGI(module_tag, "First measurement for current ambient values.");
-    ESP_LOGI(module_tag, "\tTemperature RAW value: %X", measurement_SHT85_s.temperature_raw_u8a[0]);
-    ESP_LOGI(module_tag, "\tTemperature RAW value: %X", measurement_SHT85_s.temperature_raw_u8a[1]);
-    ESP_LOGI(module_tag, "\tHumidity RAW value: %X", measurement_SHT85_s.humidity_raw_u8a[0]);
-    ESP_LOGI(module_tag, "\tHumidity RAW value: %X", measurement_SHT85_s.humidity_raw_u8a[1]);
+    
+    //ESP_LOGI(module_tag, "\tTemperature RAW value: %X", measurement_SHT85_s.temperature_raw_u8a[0]);
+    //ESP_LOGI(module_tag, "\tTemperature RAW value: %X", measurement_SHT85_s.temperature_raw_u8a[1]);
+    //ESP_LOGI(module_tag, "\tHumidity RAW value: %X", measurement_SHT85_s.humidity_raw_u8a[0]);
+    //ESP_LOGI(module_tag, "\tHumidity RAW value: %X", measurement_SHT85_s.humidity_raw_u8a[1]);
 
     measurement_SHT85_s.temperature_raw_u16 = (measurement_SHT85_s.temperature_raw_u8a[0] << 8) + (measurement_SHT85_s.temperature_raw_u8a[1]);
     
-    ESP_LOGI(module_tag, "\tTemperature RAW value: %X", measurement_SHT85_s.temperature_raw_u16);
+    //ESP_LOGI(module_tag, "\tTemperature RAW value: %X", measurement_SHT85_s.temperature_raw_u16);
     
     measurement_SHT85_s.temperature_f = -45.0f + ( 175.0f * ((float)measurement_SHT85_s.temperature_raw_u16 / 65535.0f) );
 
     ESP_LOGI(module_tag, "\tTemperature: %.2f °C", measurement_SHT85_s.temperature_f);
 
+    measurement_SHT85_s.humidity_raw_u16 = (measurement_SHT85_s.humidity_raw_u8a[0] << 8) + (measurement_SHT85_s.humidity_raw_u8a[1]);
+
+    measurement_SHT85_s.humidity_f = 100.0f * ((float)measurement_SHT85_s.humidity_raw_u16 / 65535.0f);
+    ///TODO Consider proposal of Copilot: for maximum precision, use 1.0f / 65535.0f as a constant => this avoids a division at runtime 
+
+    ESP_LOGI(module_tag, "\tHumidity: %.2f %", measurement_SHT85_s.humidity_f);
+
     //Check range of current ambient values (-40...+125°C; 0...100%)
+    if(measurement_SHT85_s.temperature_f < -40.0f)
+    {
+        //lower than -40 °C is unplausible => handle exception
+        plausibility_check_s.temperature_too_high = false;
+        plausibility_check_s.temperature_too_low = true;
+
+        ESP_LOGE(module_tag, "Plausibility check failed: Temperature too low!");
+    }
+    else
+    {
+        //greater than -40 °C is roughly in a valid range => OK, proceed
+
+        if(measurement_SHT85_s.temperature_f > 125.0f)
+        {
+            //greater than 125 °C is unplausible => handle exception
+            plausibility_check_s.temperature_too_high = true;
+            plausibility_check_s.temperature_too_low = false;
+    
+            ESP_LOGE(module_tag, "Plausibility check failed: Temperature too high!");
+        }
+        else
+        {
+            //lower than 125 °C is roughly in a valid range => OK, check passed
+            plausibility_check_s.temperature_too_high = false;
+            plausibility_check_s.temperature_too_low = false;
+            plausibility_check_s.temperature_range_check_passed = true;
+
+            ESP_LOGI(module_tag, "Plausibility check: Temperature in range (-40..125 °C)");
+        }
+
+    }
+
+    if(measurement_SHT85_s.humidity_f < 0.0f)
+    {
+        //lower than 0 % is unplausible => handle exception
+        plausibility_check_s.humidity_too_high = false;
+        plausibility_check_s.humidity_too_low = true;
+
+        ESP_LOGE(module_tag, "Plausibility check failed: Humidity too low!");
+    }
+    else
+    {
+        //greater than 0 % is roughly in a valid range => OK, proceed
+
+        if(measurement_SHT85_s.humidity_f > 100.0f)
+        {
+            //greater than 100 % is unplausible => handle exception
+            plausibility_check_s.humidity_too_high = true;
+            plausibility_check_s.humidity_too_low = false;
+
+            ESP_LOGE(module_tag, "Plausibility check failed: Humidity too high!");
+        }
+        else
+        {
+            //lower than 100 % is roughly in a valid range => OK, check passed
+            plausibility_check_s.humidity_too_high = false;
+            plausibility_check_s.humidity_too_low = false;
+            plausibility_check_s.humidity_range_check_passed = true;
+
+            ESP_LOGI(module_tag, "Plausibility check: Humidity in range (0..100 %)");
+        }
+
+    }
+
+    cmd[0] = 0xF3u;
+    cmd[1] = 0x2Du;
+    i2c_master_transmit_receive(dev_handle, cmd, 2, status_register_sht85_s.i2c_receive_data_u8a, 3, pdMS_TO_TICKS(I2C_MASTER_TIMEOUT_MS));
+
+    vTaskDelay(pdMS_TO_TICKS(I2C_MASTER_POST_FRAME_DELAY));
+
+    if( (status_register_sht85_s.i2c_receive_data_u8a[0] & 0x20u) == 0x20)
+    {
+        ESP_LOGI(module_tag, "Status Register; Heater Bit: 1");
+    }
+    else
+    {
+        ESP_LOGI(module_tag, "Status Register; Heater Bit: 0");
+    }
 
     //Enable heater
+    cmd[0] = 0x30u;
+    cmd[1] = 0x6Du;
+    i2c_master_transmit(dev_handle, cmd, 2, pdMS_TO_TICKS(I2C_MASTER_TIMEOUT_MS));
+
+    vTaskDelay(pdMS_TO_TICKS(I2C_MASTER_POST_FRAME_DELAY));
+
+    cmd[0] = 0xF3u;
+    cmd[1] = 0x2Du;
+    i2c_master_transmit_receive(dev_handle, cmd, 2, status_register_sht85_s.i2c_receive_data_u8a, 3, pdMS_TO_TICKS(I2C_MASTER_TIMEOUT_MS));
+
+    vTaskDelay(pdMS_TO_TICKS(I2C_MASTER_POST_FRAME_DELAY));
+
+    ESP_LOGI(module_tag, "Status Register MSB: %X", status_register_sht85_s.i2c_receive_data_u8a[0]);
+    ESP_LOGI(module_tag, "Status Register LSB: %X", status_register_sht85_s.i2c_receive_data_u8a[1]);
+
+    if( (status_register_sht85_s.i2c_receive_data_u8a[0] & 0x20u) == 0x20)
+    {
+        ESP_LOGI(module_tag, "Status Register; Heater Bit: 1");
+    }
+    else
+    {
+        ESP_LOGI(module_tag, "Status Register; Heater Bit: 0");
+    }
 
     //Wait 1 or 2 seconds
+    vTaskDelay(2000);
 
     //Issue single shot measurement
+    i2c_master_transmit(dev_handle, cmd, 2, pdMS_TO_TICKS(I2C_MASTER_TIMEOUT_MS));
+
+    //Wait minimum 15ms
+    vTaskDelay(pdMS_TO_TICKS(I2C_MASTER_POST_FRAME_DELAY));
+
+    //Read temperature and humidity (=> current ambient values)
+    i2c_master_receive(dev_handle, i2c_receive_data, 6, pdMS_TO_TICKS(I2C_MASTER_TIMEOUT_MS));
+
+    measurement_SHT85_s.temperature_raw_u8a[0] = i2c_receive_data[0];
+    measurement_SHT85_s.temperature_raw_u8a[1] = i2c_receive_data[1];
+    measurement_SHT85_s.crc_received_u8a[0] = i2c_receive_data[2];
+
+    measurement_SHT85_s.humidity_raw_u8a[0] = i2c_receive_data[3];
+    measurement_SHT85_s.humidity_raw_u8a[1] = i2c_receive_data[4];
+    measurement_SHT85_s.crc_received_u8a[1] = i2c_receive_data[5];
+
 
     //Wait minimum 15ms
 
     //Read temperature and humidity 
+
+    measurement_SHT85_s.temperature_raw_u16 = (measurement_SHT85_s.temperature_raw_u8a[0] << 8) + (measurement_SHT85_s.temperature_raw_u8a[1]);
+    
+    measurement_SHT85_s.temperature_f = -45.0f + ( 175.0f * ((float)measurement_SHT85_s.temperature_raw_u16 / 65535.0f) );
+
+    ESP_LOGI(module_tag, "\tTemperature: %.2f °C", measurement_SHT85_s.temperature_f);
+
+    measurement_SHT85_s.humidity_raw_u16 = (measurement_SHT85_s.humidity_raw_u8a[0] << 8) + (measurement_SHT85_s.humidity_raw_u8a[1]);
+
+    measurement_SHT85_s.humidity_f = 100.0f * ((float)measurement_SHT85_s.humidity_raw_u16 / 65535.0f);
+    ///TODO Consider proposal of Copilot: for maximum precision, use 1.0f / 65535.0f as a constant => this avoids a division at runtime 
+
+    ESP_LOGI(module_tag, "\tHumidity: %.2f %", measurement_SHT85_s.humidity_f);
+
+
+    /*
+    plausibility_check_s.heater_diff_temperature_too_low = true;
+    plausibility_check_s.heater_diff_humidity_too_low = true;
+    plausibility_check_s.heater_diff_passed = false;
+    */
 
     //Check CRC
 
@@ -385,6 +581,27 @@ static void pSHT85_LF_plausibility_check(void)
 
     
     //Disable heater
+    cmd[0] = 0x30u;
+    cmd[1] = 0x66u;
+    i2c_master_transmit(dev_handle, cmd, 2, pdMS_TO_TICKS(I2C_MASTER_TIMEOUT_MS));
+
+    vTaskDelay(pdMS_TO_TICKS(I2C_MASTER_POST_FRAME_DELAY));
+
+
+    cmd[0] = 0xF3u;
+    cmd[1] = 0x2Du;
+    i2c_master_transmit_receive(dev_handle, cmd, 2, status_register_sht85_s.i2c_receive_data_u8a, 3, pdMS_TO_TICKS(I2C_MASTER_TIMEOUT_MS));
+
+    vTaskDelay(pdMS_TO_TICKS(I2C_MASTER_POST_FRAME_DELAY));
+
+    if( (status_register_sht85_s.i2c_receive_data_u8a[0] & 0x20u) == 0x20)
+    {
+        ESP_LOGI(module_tag, "Status Register; Heater Bit: 1");
+    }
+    else
+    {
+        ESP_LOGI(module_tag, "Status Register; Heater Bit: 0");
+    }
 
     //Wait about 100ms
 
