d89ff516cf37ffcb663eb6d0d87a1fb3da7865ea
pridy_ <pridythinking@gmx.de>
2026-04-25 22:35:54 +0200

    In file pridys_SHT85.c, update comments describing public interface functions, mention recently introduced submodules and that init of these will be called; Add public function as preparation for cyclic/runtime call, function not called yet;

--- components/pridys_SHT85/pridys_SHT85.h
+++ components/pridys_SHT85/pridys_SHT85.h
@@ -10,15 +10,27 @@
 
 /**
  * Initialization function of pridys SHT85 component. 
- * Sets up I2C, reads serial number and status register.
+ * Calls init functions from submodules, 
+ * initializes module internal global variables.
  */
 void pSHT85_F_init(void);
 
 /**
  * Deinitialization function of pridys SHT85 component. 
- * Removes and deletes I2C, freeing up this channel or pins. 
+ * Calls de-init functions from submodules. 
  */
 void pSHT85_F_de_init(void);
+
+/**
+ * Cyclic/runtime function of pridys SHT85 component.
+ * Behaviour and workflow is defined here
+ *      - Read serial number of sensor SHT85
+ *      - Reset sensor SHT85
+ *      - Clear and read status register of sensor SHT85
+ *      - Do a plausibility check via heater and measurement readings
+ *      - TBD
+ */
+void pSHT85_F_cyclic(void);
 
 
 #endif /* PRIDYS_SHT85_H */
